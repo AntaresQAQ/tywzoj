63d6818292aec2a6954e0884cddb66ed7e0b6f68
Antares <antares.oier@gmail.com>
2021-12-30 21:47:02 +0800

    database

---
+++ src/database/database.providers.ts
@@ -0,0 +1,20 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { ConfigService } from '@/config/config.service';
+
+export const databaseProviders = [
+  TypeOrmModule.forRootAsync({
+    useFactory: (configService: ConfigService) => ({
+      type: configService.config.service.database.type,
+      host: configService.config.service.database.host,
+      port: configService.config.service.database.port,
+      username: configService.config.service.database.username,
+      password: configService.config.service.database.password,
+      database: configService.config.service.database.database,
+      entities: [`${__dirname}/../**/*.entity{.ts,.js}`],
+      logging: !!process.env.TYWZOJ_LOG_SQL,
+      synchronize: true,
+    }),
+    inject: [ConfigService],
+  }),
+];
