948fca3def631089faad41bd90d7099f03f1cf96
Antares <antares.oier@gmail.com>
2021-12-31 18:38:37 +0800

    import modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,38 @@
-import { forwardRef, Module } from '@nestjs/common';
+import {
+  forwardRef,
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
+
+import { AuthMiddleware } from '@/auth/auth.middleware';
+import { AuthModule } from '@/auth/auth.module';
+import { ConfigModule } from '@/config/config.module';
+import { DatabaseModule } from '@/database/database.module';
+import { UserModule } from '@/user/user.module';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from './config/config.module';
+import { ErrorFilter } from './error.filter';
+import { RedisModule } from './redis/redis.module';
 
 @Module({
-  imports: [forwardRef(() => ConfigModule)],
+  imports: [
+    ConfigModule,
+    forwardRef(() => DatabaseModule),
+    forwardRef(() => UserModule),
+    forwardRef(() => AuthModule),
+    forwardRef(() => RedisModule),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ErrorFilter],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): void {
+    consumer.apply(AuthMiddleware).forRoutes({
+      path: '*',
+      method: RequestMethod.ALL,
+    });
+  }
+}
