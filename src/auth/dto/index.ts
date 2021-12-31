fd4da57a710d661dd47295be27de9f0dc819695d
Antares <antares.oier@gmail.com>
2022-01-01 02:04:31 +0800

    register api

---
+++ src/auth/dto/index.ts
@@ -0,0 +1,4 @@
+export * from './get-session-info-request.dto';
+export * from './get-session-info-response.dto';
+export * from './register-request.dto';
+export * from './register-response.dto';
---
+++ src/auth/dto/register-response.dto.ts
@@ -0,0 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export enum RegisterResponseError {
+  ALREADY_LOGGED = 'ALREADY_LOGGED',
+  DUPLICATE_USERNAME = 'DUPLICATE_USERNAME',
+  DUPLICATE_EMAIL = 'DUPLICATE_EMAIL',
+  INVALID_EMAIL_VERIFICATION_CODE = 'INVALID_EMAIL_VERIFICATION_CODE',
+}
+
+export class RegisterResponseDto {
+  @ApiProperty({ enum: RegisterResponseError })
+  error?: RegisterResponseError;
+
+  @ApiProperty()
+  token?: string;
+}
