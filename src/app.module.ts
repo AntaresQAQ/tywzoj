527026454f4d4280f582a9f733ad75588282d488
Antares <antares.oier@gmail.com>
2022-01-01 10:48:06 +0800

    remove app.controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,38 +1,24 @@
-import {
-  forwardRef,
-  MiddlewareConsumer,
-  Module,
-  NestModule,
-  RequestMethod,
-} from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 
-import { AuthMiddleware } from '@/auth/auth.middleware';
 import { AuthModule } from '@/auth/auth.module';
 import { ConfigModule } from '@/config/config.module';
 import { DatabaseModule } from '@/database/database.module';
+import { RecaptchaModule } from '@/recaptcha/recaptcha.module';
+import { RedisModule } from '@/redis/redis.module';
 import { UserModule } from '@/user/user.module';
 
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ErrorFilter } from './error.filter';
-import { RedisModule } from './redis/redis.module';
 
 @Module({
   imports: [
     ConfigModule,
+    RecaptchaModule,
     forwardRef(() => DatabaseModule),
     forwardRef(() => UserModule),
     forwardRef(() => AuthModule),
     forwardRef(() => RedisModule),
   ],
-  controllers: [AppController],
   providers: [AppService, ErrorFilter],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer): void {
-    consumer.apply(AuthMiddleware).forRoutes({
-      path: '*',
-      method: RequestMethod.ALL,
-    });
-  }
-}
+export class AppModule {}
