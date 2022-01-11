3e8c784def3a355c65acdbaaa344810af8448d58
Antares <antares.oier@gmail.com>
2022-01-11 23:48:15 +0800

    fix

--- src/auth/dto/send-verification-code-response.dto.ts
+++ src/auth/dto/send-verification-code-response.dto.ts
@@ -2,7 +2,7 @@ import { ApiProperty } from '@nestjs/swagger';
 
 export enum SendVerificationCodeResponseError {
   PERMISSION_DENIED = 'PERMISSION_DENIED', // Change email
-  DUPLICATE_EMAIL = 'DUPLICATE_EMAIL', // Change email
+  DUPLICATE_EMAIL = 'DUPLICATE_EMAIL', // Change email and Register
   NO_SUCH_USER = 'NO_SUCH_USER', // Reset password
   ALREADY_LOGGED = 'ALREADY_LOGGED', // Register
   FAILED_TO_SEND = 'FAILED_TO_SEND',
