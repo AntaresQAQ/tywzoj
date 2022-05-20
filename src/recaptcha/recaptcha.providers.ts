91154ba0d836fe8a7b7769f30e29b206ee608027
Antares <antares.oier@gmail.com>
2022-05-20 14:25:41 +0800

    fix bug

--- src/recaptcha/recaptcha.providers.ts
+++ src/recaptcha/recaptcha.providers.ts
@@ -1,4 +1,4 @@
-import { GoogleRecaptchaModule } from '@nestlab/google-recaptcha';
+import { GoogleRecaptchaModule, GoogleRecaptchaNetwork } from '@nestlab/google-recaptcha';
 
 import { RequestWithSession } from '@/auth/auth.middleware';
 import { ConfigModule } from '@/config/config.module';
@@ -11,6 +11,9 @@ export const recaptchaProviders = [
       secretKey: configService.config.security.recaptcha.secretKey,
       response: (req: RequestWithSession) => String(req.headers['x-recaptcha-token']),
       skipIf: () => !configService.config.preference.security.recaptchaEnabled,
+      network: configService.config.security.recaptcha.useRecaptchaNet
+        ? GoogleRecaptchaNetwork.Recaptcha
+        : GoogleRecaptchaNetwork.Google,
     }),
     inject: [ConfigService],
   }),
