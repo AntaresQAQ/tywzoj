c100b6f67a56ef450527b8f4c0c444ed7b5ac675
Antares <antares.oier@gmail.com>
2022-06-02 19:24:20 +0800

    rate limiter whitelist

--- src/rate-limiter.middleware.ts
+++ src/rate-limiter.middleware.ts
@@ -1,5 +1,6 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response } from 'express';
+import { cidrSubnet } from 'ip';
 import { RateLimiterAbstract, RateLimiterMemory } from 'rate-limiter-flexible';
 
 import { ConfigService } from '@/config/config.service';
@@ -7,6 +8,7 @@ import { ConfigService } from '@/config/config.service';
 @Injectable()
 export class RateLimiterMiddleware implements NestMiddleware {
   private readonly rateLimiter: RateLimiterAbstract;
+
   constructor(private readonly configService: ConfigService) {
     if (this.configService.config.security.rateLimit.enabled) {
       this.rateLimiter = new RateLimiterMemory({
@@ -15,8 +17,15 @@ export class RateLimiterMiddleware implements NestMiddleware {
       });
     }
   }
+
   use(req: Request, res: Response, next: () => void) {
     if (this.rateLimiter) {
+      for (const cidr of this.configService.config.security.rateLimit.whiteList) {
+        if (cidrSubnet(cidr).contains(req.ip)) {
+          next();
+          return;
+        }
+      }
       this.rateLimiter
         .consume(req.ip)
         .then(() => next())
