eab5fc3dc6b65a21152a92d787f748b011f353c3
Antares <antares.oier@gmail.com>
2022-06-02 19:21:44 +0800

    isCIDR validator

--- src/common/validators.ts
+++ src/common/validators.ts
@@ -1,6 +1,6 @@
 // https://github.com/lyrio-dev/lyrio/blob/33c4ac58857d15a15d0f529354ceee69fc86279b/src/common/validators.ts
 
-import { registerDecorator, ValidationOptions } from 'class-validator';
+import { isIP, registerDecorator, ValidationOptions } from 'class-validator';
 
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 
@@ -22,8 +22,8 @@ export function If<T>(
   };
 }
 
-// class-validator's IsNumberString accepts floating numbers only, but I
-// want to validate if it's a integer
+// class-validator's IsNumberString accepts floating numbers only,
+// but I want to validate if it's an integer
 export function IsIntString(validationOptions?: ValidationOptions) {
   return If(
     value => typeof value === 'string' && Number.isInteger(Number(value)),
@@ -31,8 +31,8 @@ export function IsIntString(validationOptions?: ValidationOptions) {
   );
 }
 
-// class-validator's IsPort accepts strings only, but I prefer
-// writing port numbers as number
+// class-validator's IsPort accepts strings only,
+// but I prefer writing port numbers as number
 export function IsPortNumber(validationOptions?: ValidationOptions) {
   return If(
     value => Number.isInteger(value) && value >= 1 && value <= 65535,
@@ -51,16 +51,6 @@ export function IsUsername(validationOptions?: ValidationOptions) {
   return If(value => typeof value === 'string' && isUsername(value), validationOptions);
 }
 
-// A group name is a string of 1 ~ 48 ASCII characters, and each character
-// is a uppercase / lowercase letter or a number or any of ' :@~-_.#$/'
-// and is NOT '%'.
-export function IsGroupName(validationOptions?: ValidationOptions) {
-  return If(
-    value => typeof value === 'string' && /^[a-zA-Z0-9 :@~\-_.#$/]{1,48}$/.test(value),
-    validationOptions,
-  );
-}
-
 export function isValidFilename(filename: string): boolean {
   const forbiddenCharacters = ['/', '\x00'];
   const reservedFilenames = ['.', '..'];
@@ -75,4 +65,19 @@ export function IsValidFilename(validationOptions?: ValidationOptions) {
     value => typeof value === 'string' && isValidFilename(value),
     validationOptions,
   );
+}
+
+export function isCIDR(value: string): boolean {
+  const parts = value.split('/');
+  if (parts.length != 2) return false;
+  const mask = Number(parts[1]);
+  return (
+    Number.isInteger(mask) &&
+    ((isIP(parts[0], 4) && mask >= 0 && mask <= 32) ||
+      (isIP(parts[0], 6) && mask >= 0 && mask <= 128))
+  );
+}
+
+export function IsCIDR(validationOptions?: ValidationOptions) {
+  return If(value => typeof value === 'string' && isCIDR(value), validationOptions);
 }
