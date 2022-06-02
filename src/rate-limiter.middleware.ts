080d5f5cad9551bdd9abd33253d61901d25c672d
Antares <antares.oier@gmail.com>
2022-06-02 19:43:11 +0800

    optimize code

--- src/rate-limiter.middleware.ts
+++ src/rate-limiter.middleware.ts
@@ -1,35 +1,39 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response } from 'express';
-import { cidrSubnet } from 'ip';
+import { cidrSubnet, SubnetInfo } from 'ip';
 import { RateLimiterAbstract, RateLimiterMemory } from 'rate-limiter-flexible';
 
 import { ConfigService } from '@/config/config.service';
 
 @Injectable()
 export class RateLimiterMiddleware implements NestMiddleware {
+  private readonly enabled: boolean;
   private readonly rateLimiter: RateLimiterAbstract;
+  private readonly whitelist: SubnetInfo[];
 
   constructor(private readonly configService: ConfigService) {
-    if (this.configService.config.security.rateLimit.enabled) {
+    this.enabled = this.configService.config.security.rateLimit.enabled;
+    if (this.enabled) {
       this.rateLimiter = new RateLimiterMemory({
         points: this.configService.config.security.rateLimit.maxRequests,
         duration: this.configService.config.security.rateLimit.durationSeconds,
       });
+      this.whitelist = this.configService.config.security.rateLimit.whitelist.map(value =>
+        cidrSubnet(value),
+      );
     }
   }
 
   use(req: Request, res: Response, next: () => void) {
-    if (this.rateLimiter) {
-      for (const cidr of this.configService.config.security.rateLimit.whitelist) {
-        if (cidrSubnet(cidr).contains(req.ip)) {
-          next();
-          return;
-        }
+    if (this.enabled) {
+      if (this.whitelist.find(cidr => cidr.contains(req.ip))) {
+        next();
+      } else {
+        this.rateLimiter
+          .consume(req.ip)
+          .then(() => next())
+          .catch(() => res.status(429).send('Too Many Requests'));
       }
-      this.rateLimiter
-        .consume(req.ip)
-        .then(() => next())
-        .catch(() => res.status(429).send('Too Many Requests'));
     } else {
       next();
     }
