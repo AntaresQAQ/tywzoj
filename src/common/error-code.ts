fc90a3821de311fa0ba3135bb4ceb35763269a4f
Antares <antares.mi@outlook.com>
2023-01-18 21:33:15 +0800

    add errors

--- src/common/error-code.ts
+++ src/common/error-code.ts
@@ -4,6 +4,15 @@ export const enum CE_ErrorCode {
   PermissionDenied = 403,
 
   // Auth (50xx)
+  Auth_NoSuchUser = 5000,
+  Auth_WrongPassword = 5001,
+  Auth_AlreadyLoggedIn = 5002,
+  Auth_NotLoggedIn = 5003,
+  Auth_DuplicateUsername = 5004,
+  Auth_DuplicateEmail = 5005,
+  Auth_InvalidEmailVerificationCode = 5006,
+  Auth_FailedToSendEmailVerificationCode = 5007,
+  Auth_EmailVerificationCodeRateLimited = 5008,
 
   // User (51xx)
   User_TakeTooMany = 5100,
